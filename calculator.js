72356e6cca0fe514ff1e38016baed69fac71352d
muhammad-danish-webdev <muhammaddanish.webdev@gmail.com>
2026-04-04 17:24:29 +0500

    Code Added

--- calculator.js
+++ calculator.js
@@ -1,3 +1,19 @@
 let dispaly = document.getElementById("display");
-let buttons = document.getElementsByClassName("button");
-console.log(buttons);
+let buttons = Array.from(document.getElementsByClassName("button"));
+
+// buttons.map((button) => {
+//   button.addEventListener("click", (e) => {
+//     // console.log("clicked");
+//     // console.log(e);
+//     // console.log(e.target);
+//     // console.log(e.target.innerText);
+//   });
+// });
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.innerText) {
+      default:
+        dispaly.innerText += e.target.innerText;
+    }
+  });
+});
